d262e9758344f83be26e051722fcbc401317a679
Abzal Tuganbay <abzal.tugan@gmail.com>
2022-04-06 10:59:04 +0600

    modify

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 
 func f(i, i2 int) {
 	n := time.Now()
+	fmt.Println(i, "  =========", i2)
 	for i < i2 {
 		v := strconv.Itoa(i)
 		var jsonData = []byte(fmt.Sprintf(`"body"{"phone":"%s"}`, v))
@@ -37,12 +38,65 @@ func getAll(w http.ResponseWriter, r *http.Request) {
 	var jsonMap map[string]int
 	b, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(b, &jsonMap)
-	t1 := fmt.Sprintf("7%d0000000", jsonMap["code"])
-	t2 := fmt.Sprintf("7%d0000100", jsonMap["code"])
 	flag.Parse()
-	i, _ := strconv.Atoi(t1)
-	i2, _ := strconv.Atoi(t2)
-	go f(i, i2)
+
+	for x := 0; x < 9; x++ {
+		var t1, t2 string
+		if x > 0 && x < 9 {
+			t1 = fmt.Sprintf("7%d0000%d00", jsonMap["code"], x)
+			t2 = fmt.Sprintf("7%d0000%d00", jsonMap["code"], x+1)
+		}
+		i, _ := strconv.Atoi(t1)
+		i2, _ := strconv.Atoi(t2)
+		fmt.Println("Goroutine:", x, " range: ", x, x+1)
+		go f(i, i2)
+	}
+
+	for x := 10; x < 99; x++ {
+		var t1, t2 string
+
+		t1 = fmt.Sprintf("7%d000%d00", jsonMap["code"], x)
+		t2 = fmt.Sprintf("7%d000%d00", jsonMap["code"], x+1)
+
+		i, _ := strconv.Atoi(t1)
+		i2, _ := strconv.Atoi(t2)
+		fmt.Println("Goroutine:", x, " range: ", x, x+1)
+		go f(i, i2)
+	}
+	for x := 100; x < 999; x++ {
+		var t1, t2 string
+		if x < 999 && x > 99 {
+			t1 = fmt.Sprintf("7%d00%d00", jsonMap["code"], x)
+			t2 = fmt.Sprintf("7%d00%d00", jsonMap["code"], x+1)
+		}
+		i, _ := strconv.Atoi(t1)
+		i2, _ := strconv.Atoi(t2)
+		fmt.Println("Goroutine:", x, " range: ", x, x+1)
+		go f(i, i2)
+	}
+	for x := 1000; x < 9999; x++ {
+		var t1, t2 string
+		if x < 9999 && x > 999 {
+			t1 = fmt.Sprintf("7%d0%d00", jsonMap["code"], x)
+			t2 = fmt.Sprintf("7%d0%d00", jsonMap["code"], x+1)
+		}
+		i, _ := strconv.Atoi(t1)
+		i2, _ := strconv.Atoi(t2)
+		fmt.Println("Goroutine:", x, " range: ", x, x+1)
+		go f(i, i2)
+	}
+	for x := 10000; x < 99999; x++ {
+		var t1, t2 string
+		if x < 99999 && x > 9999 {
+			t1 = fmt.Sprintf("7%d%d00", jsonMap["code"], x)
+			t2 = fmt.Sprintf("7%d%d00", jsonMap["code"], x+1)
+		}
+		i, _ := strconv.Atoi(t1)
+		i2, _ := strconv.Atoi(t2)
+		fmt.Println("Goroutine:", x, " range: ", x, x+1)
+		go f(i, i2)
+	}
+	fmt.Println("Done")
 }
 
 func getOne(w http.ResponseWriter, r *http.Request) {
